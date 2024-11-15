4efaee351a62d355e3a754f66bb57a370d1194e6
camilajaviera <cjmunozn@hotmail.com>
2024-11-14 23:04:45 -0300

    Initialize the API and authenticate

--- kaggle_connect.py
+++ kaggle_connect.py
@@ -2,4 +2,9 @@ from kaggle.api.kaggle_api_extended import KaggleApi as ka
 import pandas as pd
 from pathlib import Path
 
+#"Download" base path
 base_folder = Path("./")
+
+#Initialize the API and authenticate
+api = ka()
+api.authenticate()
