aef45850662541ed0395ba6911a4fa45e7ff7c49
camilajaviera <cjmunozn@hotmail.com>
2024-11-14 23:35:42 -0300

    fix(kaggle_connect): change the code into a function and return the DataFrame (df). So it can be used in other files

--- kaggle_connect.py
+++ kaggle_connect.py
@@ -2,42 +2,46 @@ from kaggle.api.kaggle_api_extended import KaggleApi as ka
 import pandas as pd
 from pathlib import Path
 
-#"Download" base path
-base_folder = Path("./")
+def kaggle_connect():
 
-#Initialize the API and authenticate
-api = ka()
-api.authenticate()
+    #"Download" base path
+    base_folder = Path("./")
 
-#Prompt for the search term
-search_term = input("Search for data: ")
+    #Initialize the API and authenticate
+    api = ka()
+    api.authenticate()
 
-#List datasets related to the search term
-datasets = api.dataset_list(search=search_term)
+    #Prompt for the search term
+    search_term = input("Search for data: ")
 
-for dataset in datasets:
-    print("Dataset found:", dataset.ref)
+    #List datasets related to the search term
+    datasets = api.dataset_list(search=search_term)
 
-#Dataset selection
-data_ref = input("Enter Option (dataset ref): ")
+    for dataset in datasets:
+        print("Dataset found:", dataset.ref)
 
-#Destination folder for the download
-new_folder = input("Enter the name of the new folder to store the dataset: ")
-download_path = base_folder / new_folder
+    #Dataset selection
+    data_ref = input("Enter Option (dataset ref): ")
 
-#Create the folder if it doesn't exist
-download_path.mkdir(parents=True, exist_ok=True)
+    #Destination folder for the download
+    new_folder = input("Enter the name of the new folder to store the dataset: ")
+    download_path = base_folder / new_folder
 
-#Download the dataset and unzip it in the specified folder
-api.dataset_download_files(data_ref, path=str(download_path), unzip=True)
+    #Create the folder if it doesn't exist
+    download_path.mkdir(parents=True, exist_ok=True)
 
-#List all CSV files in the download directory
-csv_files = list(download_path.glob('*.csv'))
-if not csv_files:
-    print("No CSV files found in the dataset.")
-else:
-    #Select the first CSV file in the directory
-    csv_file = csv_files[0]
+    #Download the dataset and unzip it in the specified folder
+    api.dataset_download_files(data_ref, path=str(download_path), unzip=True)
 
-#Load the dataset into a DataFrame
-    df = pd.read_csv(csv_file)
+    #List all CSV files in the download directory
+    csv_files = list(download_path.glob('*.csv'))
+    if not csv_files:
+        print("No CSV files found in the dataset.")
+    else:
+        #Select the first CSV file in the directory
+        csv_file = csv_files[0]
+
+    #Load the dataset into a DataFrame
+        df = pd.read_csv(csv_file)
+    
+    return df
