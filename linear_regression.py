e4db60bcbd9fe0aa99b752776eb5cde3dcafe754
camilajaviera <cjmunozn@hotmail.com>
2024-11-18 19:31:57 -0300

    del(linear_regression): deleting part of the code that won't be needed for now

--- linear_regression.py
+++ linear_regression.py
@@ -4,7 +4,3 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import OrdinalEncoder as oe
 from sklearn.linear_model import LinearRegression as lr
 from kaggle_connect import kaggle_connect as kc
-
-#1 Step: Create a new def where it calls the DataFrame
-def load_data():
-    return kc()
