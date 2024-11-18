e9df5cabca77b5161b82831abbfed51bd09fd550
camilajaviera <cjmunozn@hotmail.com>
2024-11-18 19:46:59 -0300

    feat(linear_regression): create a main flow so it can call the DataFrame (also, pandas as od was changed to pandas as pd)

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,13 @@
-import pandas as od
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import OrdinalEncoder as oe
 from sklearn.linear_model import LinearRegression as lr
 from kaggle_connect import kaggle_connect as kc
+
+#Main
+data = kc()
+df = pd.DataFrame(data)
+
+# Show the number of unique values per column
+print(df.nunique())
