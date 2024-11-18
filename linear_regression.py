40864cf53009215eeb0cb7bebed635de521596a8
camilajaviera <cjmunozn@hotmail.com>
2024-11-18 19:12:58 -0300

    feat(linear_regression): #1 Step: Create a new def where it calls the DataFrame

--- linear_regression.py
+++ linear_regression.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import OrdinalEncoder as oe
 from sklearn.linear_model import LinearRegression as lr
 from kaggle_connect import kaggle_connect as kc
+
+#1 Step: Create a new def where it calls the DataFrame
+def load_data():
+    return kc()
